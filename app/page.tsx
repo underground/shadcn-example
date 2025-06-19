fab84dc9089105611403e327a4cbdbcbe7f451bf
nakanori <nacanori@gmail.com>
2025-06-19 20:36:41 +0900

    add input field

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
@@ -50,6 +51,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <Input placeholder="Type something..." />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
