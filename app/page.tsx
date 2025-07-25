e81196c5b685cbae0c2c14c247adfb1d48472bf6
nakanori <nacanori@gmail.com>
2025-07-25 09:50:52 +0900

    remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import CameraOverlay from "@/app/CameraOverlay";
