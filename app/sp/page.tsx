8ef5987bf6b3e17ddae109c993c1b4ae8d006561
nakanori <nacanori@gmail.com>
2025-09-30 18:35:25 +0900

    fixed header

--- app/sp/page.tsx
+++ app/sp/page.tsx
@@ -1,16 +1,32 @@
 import { Input } from "@/components/ui/input";
+import clsx from "clsx";
 
 export default function Page() {
   return (
     <form
+      className={clsx(
+        // 画面全体
+        "min-h-[100svh]",
+        "[@supports(height:100dvh)]:min-h-[100dvh]",
+        // ヘッダー / メイン / フッター の3行
+        "grid grid-rows-[auto,1fr,auto]"
+      )}
       // className="h-full w-full flex flex-col overflow-hidden fixed top-0 bottom-0 left-0 right-0"
-      className="min-h-[100svh] [@supports(height:100dvh)]:min-h-[100dvh] grid grid-rows-[auto,1fr,auto]"
     >
-      <header className="flex flex-col w-full h-[50px] bg-black text-white">
+      <header
+        className={clsx(
+          "sticky top-0 z-10 flex flex-col w-full",
+          "h-[50px] bg-black text-white flex items-center px-4 shadow"
+        )}
+      >
         Header
       </header>
       <main
-        className=" overflow-auto px-4 py-3 min-h-0 scroll-padding-bottom-[84px] gap-2"
+        className={clsx(
+          "overflow-auto px-4 py-3",
+          "flex flex-col gap-2", // 入力の間に gap-2
+          "min-h-0" // flex 子がはみ出さないように必須
+        )}
         // className="flex flex-col flex-auto overflow-auto no-scrollbar overflow-x-hidden gap-2 h-full p-3"
       >
         {Array.from({ length: 30 }).map((_, i) => (
@@ -21,8 +37,10 @@ export default function Page() {
         className="
     sticky bottom-0 inset-x-0
     bg-black text-white
-    px-4 py-3 shadow-2xl
-    h-[50px]
+    shadow-2xl
+    min-h-[60px]
+    max-h-[60px]
+    h-[60px]
   "
         // className="bg-black text-white p-3 mt-auto text-center"
       >
