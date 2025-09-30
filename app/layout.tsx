9084799f932b88ca989abcbb5f92c4b892281cdd
nakanori <nacanori@gmail.com>
2025-09-30 18:08:52 +0900

    interactiveWidget: "resizes-content"

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
+  interactiveWidget: "resizes-content",
 };
 
 export const metadata: Metadata = {
